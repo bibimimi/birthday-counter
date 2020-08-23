2521381fd8464280481b5ede62ca741a939dd640
Trisha Cabildo <Trisha.Cabildo@analog.com>
2020-08-23 21:43:27 +0800

    change name

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Sept 06, 2020',
+  name: 'Louijie'
 };
 
 function hideEverything() {
